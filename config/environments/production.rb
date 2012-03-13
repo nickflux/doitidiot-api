4f8a5293c616f0bd94d412bd6059c0e07123aff5
nickflux <nick@bolsovernetworks.com>
2012-03-13 09:05:05 +0000

    add SendGrid

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -38,7 +38,7 @@ DoitidiotApi::Application.configure do
   # config.logger = SyslogLogger.new
 
   # Use a different cache store in production
-  config.cache_store = :dalli_store
+  config.cache_store = :dalli_store, { :expires_in => 1.day, :compress => true }
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server
   # config.action_controller.asset_host = "http://assets.example.com"
