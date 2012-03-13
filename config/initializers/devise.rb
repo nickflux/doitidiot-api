9be96ac1721a6c14bc704d62902976b023e251cf
nickflux <nick@bolsovernetworks.com>
2012-03-13 12:46:36 +0000

    Try to get omniauth working

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -196,7 +196,8 @@ Devise.setup do |config|
   # ==> OmniAuth
   # Add a new OmniAuth provider. Check the wiki for more information on setting
   # up on your models and hooks.
-  config.omniauth :twitter, 'D1NiIFC2GthXodAo7hg', '8POjFFyK6jG2ZobBkfo9BpbEeY1JU72eRhEo36t3o'
+  equire "omniauth-twitter"
+  config.omniauth :twitter, 'D1NiIFC2GthXodAo7hg', '8POjFFyK6jG2ZobBkfo9BpbEeY1JU72eRhEo36t3o', :strategy_class => OmniAuth::Strategies::Twitter
 
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
